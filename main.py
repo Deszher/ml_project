63b6c0ba056018af135c205731111a1ff0ce7b66
Deszher <143352282+Deszher@users.noreply.github.com>
2024-02-21 00:12:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from transformers import pipeline
 pipe = pipeline("translation", model="WelfCrozzo/T5-L128-belarusian")
 
 TEXT = "Не ищи счастье – оно всегда у тебя внутри"
-print(f"'{TEXT}' в переводе на белорусский язык означает - '{pipe(text, max_length=50)[0]['translation_text']}'")
+print(f"'{TEXT}' в переводе на белорусский язык означает - '{pipe(TEXT, max_length=50)[0]['translation_text']}'")
